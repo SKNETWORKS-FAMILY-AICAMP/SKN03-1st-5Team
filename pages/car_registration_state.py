fba62675ad625b325178ad91ecff975983742594
singas0ng <031212sms@gmail.com>
2024-07-25 11:15:17 +0900

    add data

--- pages/car_registration_state.py
+++ pages/car_registration_state.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 st.title('전국 자동차 등록 현황')
 
@@ -17,25 +18,32 @@ sql = """
 """
 
 df = conn.query(sql, ttl=3600)
-tab1, tab2 = st.tabs(["📈 자가용", "📈 공업용"])
+tab1, tab2, tab3 = st.tabs(["🚗 자가용", "🚚 공업용", "🚥 종합 현황"])
 if not df.empty:
     st.dataframe(df)
 
-    car_column1 = 'city' 
-    number_of_cars_column1 = 'own_car' 
+    car_column = 'city' 
+    number_of_cars_column1 = 'own_car'
+    number_of_cars_column2 = 'offical_car'
+    total = 'total' 
     
-    car_column2 = 'city' 
-    number_of_cars_column2 = 'offical_car' 
-    
-    data1 = df.set_index(car_column1)[number_of_cars_column1]
-    data2 = df.set_index(car_column2)[number_of_cars_column2]
+    data1 = df.set_index(car_column)[number_of_cars_column1]
+    data2 = df.set_index(car_column)[number_of_cars_column2]
+    data3 = df.set_index(car_column)[[number_of_cars_column1 ,number_of_cars_column2, total]]
 
-    tab1.subheader("지역별 자가용 보유 현황")
-    tab1.line_chart(data1)
+    tab1.subheader("지역별 공업용 보유 현황")
+    tab1.line_chart(data = data1, color='#df7aaf')
 
-    tab2.subheader("지역별 공업용 보유 현황")
-    tab2.line_chart(data2)
+    tab2.subheader("지역별 자가용 보유 현황")
+    tab2.line_chart(data = data2, color = '#bada55')
+
+    tab3.subheader('지역별 종합 보유 현황')
+    tab3.line_chart(data=data3, color = ['#bada55','#df7aaf','#7aa5c1'] )
 
 else:
     tab1.subheader("No data available to display.")
     tab2.subheader("No data available to display.")
+    tab3.subheader("No data available to display.")
+
+
+
